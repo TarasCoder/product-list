0cc2d289936099bf707fdffbb44b946373a40b41
kaskad kaskad <4kaskad@gmail.com>
2023-04-04 16:53:19 +0300

    adding styling to reading comments component

--- src/components/Modals/Comments/ReadComments/ReadComments.jsx
+++ src/components/Modals/Comments/ReadComments/ReadComments.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import "./ReadComments.css"
+import React from "react";
+import "./ReadComments.css";
 
 function ReadComments(props) {
-
-    const closeModal = () => {
-        props.closeReadComments();
-    }
+  const closeModal = () => {
+    props.closeReadComments();
+  };
 
   return (
     <div className="add_modal add_modal_modify modal_readMore">
-      <h2 className="modal_caption modal_caption_modify modal_readMore_caption">Read Comments</h2>
-      {props.comments.map((msg, index) => <p key={index} className="comment">{msg}</p>)}
+      <h2 className="modal_caption modal_caption_modify modal_readMore_caption">
+        Read Comments
+      </h2>
+      <div className="comments-block">
+        {props.comments.map((msg, index) => (
+          <p key={index} className="comment">
+            {msg}
+          </p>
+        ))}
+      </div>
       <div className="modal_buttons modal_buttons_modify">
         <button className="btn_close" onClick={closeModal}>
           Close
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default ReadComments;
