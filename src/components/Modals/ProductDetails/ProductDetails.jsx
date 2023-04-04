9f111dd012b7bf10d6c170f40b86afef7c0655d0
kaskad kaskad <4kaskad@gmail.com>
2023-04-04 17:38:15 +0300

    while pressing edit button adding switch between edit and read modes

--- src/components/Modals/ProductDetails/ProductDetails.jsx
+++ src/components/Modals/ProductDetails/ProductDetails.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import tempBox from "../../../images/package.png";
 import "./ProductDetails.css";
 
 function ProductDetails(props) {
+  const [editMode, setEditMode] = useState(false);
   const itemData = props.productDetailsItemData;
   const ava = itemData.image == null ? tempBox : itemData.image;
   const closeProductDetails = () => {
     props.closeProductDetails();
   };
-  const editMode = () => {
-    console.log("Edit mode!");
+  const saveEditedCard = () => {
+    console.log("Save edited card!");
+    setEditMode(false);
+  };
+  const editCard = () => {
+    setEditMode(true);
   };
   const addComment = () => {
     props.openAddComments();
@@ -40,26 +45,30 @@ function ProductDetails(props) {
           <label htmlFor="name" className="modal_label_itemData">
             Name:
           </label>
-          <h4 className="itemData">{itemData.name}</h4>
-          {/* <input
-            name="name"
-            type="text"
-            className="modal_input"
-            value={}
-          /> */}
+          {!editMode && <h4 className="itemData">{itemData.name}</h4>}
+          {editMode && (
+            <input
+              name="name"
+              type="text"
+              className="modal_input"
+              value={itemData.name}
+            />
+          )}
         </div>
         {/* Quantity input */}
         <div className="modal_input_block">
           <label htmlFor="quantity" className="modal_label_itemData">
             Quantity:
           </label>
-          <h4 className="itemData">{itemData.quantity}</h4>
-          {/* <input
-            name="quantity"
-            type="number"
-            className="modal_input"
-            value={}
-          /> */}
+          {!editMode && <h4 className="itemData">{itemData.quantity}</h4>}
+          {editMode && (
+            <input
+              name="quantity"
+              type="number"
+              className="modal_input"
+              value={itemData.quantity}
+            />
+          )}
         </div>
 
         {/* Description input */}
@@ -67,22 +76,32 @@ function ProductDetails(props) {
           <label htmlFor="description" className="modal_label_itemData">
             Description:
           </label>
-          <div className="modal_label_itemData modal_label_itemData_description">
-            <p className="itemData itemData_description" name="description">
-              {description.length < NUM_OF_SYMBOLS
-                ? description
-                : descriptionSliced + continueSighn}
-            </p>
-            {description.length > NUM_OF_SYMBOLS && (
-              <a
-                className="modal_label_itemData_description_link"
-                href="#"
-                onClick={show_more}
-              >
-                show more
-              </a>
-            )}
-          </div>
+          {!editMode && (
+            <div className="modal_label_itemData modal_label_itemData_description">
+              <p className="itemData itemData_description" name="description">
+                {description.length < NUM_OF_SYMBOLS
+                  ? description
+                  : descriptionSliced + continueSighn}
+              </p>
+              {description.length > NUM_OF_SYMBOLS && (
+                <a
+                  className="modal_label_itemData_description_link"
+                  href="#"
+                  onClick={show_more}
+                >
+                  show more
+                </a>
+              )}
+            </div>
+          )}
+          {editMode && (
+            <input
+              name="description"
+              type="text"
+              className="modal_input"
+              value={itemData.description}
+            />
+          )}
         </div>
 
         {/* <div className="modal_input_block">
@@ -102,15 +121,21 @@ function ProductDetails(props) {
           <label htmlFor="image" className="modal_label_itemData">
             Image:
           </label>
-          <div className="modal_label_itemData modal_label_itemData_img">
-            <img src={ava} alt="ava" className="modal_ava modal_ava" />
-          </div>
-          {/* <input
-            name="image"
-            type="file"
-            className="modal_input modal_input_ava"
-            value={}
-          /> */}
+          {!editMode && (
+            <div className="modal_label_itemData modal_label_itemData_img">
+              <img src={ava} alt="ava" className="modal_ava modal_ava" />
+            </div>
+          )}
+          {editMode && (
+            <div className="edit_ava">
+              <input
+                name="image"
+                type="file"
+                className="modal_input modal_input_ava"
+                value={itemData.image}
+              />
+            </div>
+          )}
         </div>
         {/* Comments section */}
         <div className="modal_input_block">
@@ -118,10 +143,16 @@ function ProductDetails(props) {
             Comments:
           </label>
           <div className="modal_label_itemData modal_label_itemData_comments">
-            <h4 className="itemDataLengthComments">{itemData.comments.length}</h4>
+            <h4 className="itemDataLengthComments">
+              {itemData.comments.length}
+            </h4>
             <div>
-              <p className="itemDataComments" onClick={addComment}>add comment</p>
-              <p className="itemDataComments" onClick={readComment}>read comment(s)</p>
+              <p className="itemDataComments" onClick={addComment}>
+                add comment
+              </p>
+              <p className="itemDataComments" onClick={readComment}>
+                read comment(s)
+              </p>
             </div>
           </div>
           {/* <input
@@ -133,9 +164,16 @@ function ProductDetails(props) {
         </div>
       </div>
       <div className="modal_buttons_product_details">
-        <button type="submit" className="btn_edit" onClick={editMode}>
-          Edit
-        </button>
+        {!editMode && (
+          <button type="submit" className="btn_edit" onClick={editCard}>
+            Edit
+          </button>
+        )}
+        {editMode && (
+          <button type="submit" className="btn_edit" onClick={saveEditedCard}>
+            Save
+          </button>
+        )}
         <button type="button" className="btn_close" onClick={handleDelete}>
           Delete
         </button>
